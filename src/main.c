fe4134c01c3227e15541871622fe55dead3ab293
leofigueredo2008-beep <leofigueredo.2008@gmail.com>
2026-04-24 10:32:39 -0300

    Validando notas dadas pelo o usuário

--- src/main.c
+++ src/main.c
@@ -3,12 +3,25 @@
 float media; 
 float nota1, nota2; 
 
+float validar_nota(nota){
+    float nota_aux = nota;
+
+    while(nota_aux<0 || nota_aux>10){
+        printf("O valor digitado deve ser entre 0 e 10.\n");
+        printf("Digite a nota novamente: ");
+        scanf("%f", &nota_aux);
+    }
+
+    return nota_aux;
+}
+
 float calcular_media(){
     printf("Digite a primeira nota: ");
     scanf("%f", &nota1);
+    nota1 = validar_nota(nota1);
     printf("Digite a segunda nota: ");
     scanf("%f", &nota2);
-
+    nota2 = validar_nota(nota2);
 }
 
 
