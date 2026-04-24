d7ba39d5d69857db8bf5774d45def5966173627b
LuRSousa <lucasrs.tech@gmail.com>
2026-04-24 11:23:32 -0300

    Adicionado switch para funcionalidade do menu e correção de alguns bugs

--- src/main.c
+++ src/main.c
@@ -12,15 +12,16 @@ int menu(){
     printf("\nSISTEMA EQUIPE LCE\n");
     printf("\n==================\n");
 
-    printf("Selecione o que deseja fazer: \n1- Calcular Media \n2- Calcular Derivada \n3- Sair \n");
+    printf("Selecione o que deseja fazer: \n1- Calcular Media \n2- Verificar Situacao do Aluno \n3- Calcular Derivada \n4- Sair \n");
     printf("Opcao: ");
     scanf("%d", &opcao);
 
-    while(opcao < 1 || opcao > 3){
-        printf("\nValor Invalido! O valor deve estar entre 1 e 3.\n");
-        printf("Selecione novamente o que deseja fazer: \n1- Calcular Media \n2- Calcular Derivada \n3- Sair\n");
+    while(opcao < 1 || opcao > 4){
+        printf("\nValor Invalido! O valor deve estar entre 1 e 4.\n");
+        printf("Selecione novamente o que deseja fazer: \n1- Calcular Media \n2- Verificar Situação do Aluno \n3- Calcular Derivada \n4- Sair \n");
         printf("Opcao: ");
         scanf("%d", &opcao);
+        printf("\n");
     }
 
     return opcao;
@@ -51,7 +52,7 @@ void calcular_derivada(){
     }
 }
 
-float validar_nota(nota){
+float validar_nota(float nota){
     float nota_aux = nota;
 
     while(nota_aux<0 || nota_aux>10){
@@ -77,18 +78,18 @@ float calcular_media(){
     return media;
 }
 
-int situacao_academica(media) {
+int situacao_academica(float media) { 
+    if(!media){
+        printf("Media nao calculada");
+        return 0;
+    }
+    
     printf("Digite a presenca do aluno: ");
     scanf("%f", &presenca);
-    while(presenca<0 || presenca>10){
+    while(presenca<0 || presenca>100){
         printf("O valor digitado deve ser entre 0 e 100.\n");
         printf("Digite a presenca novamente: ");
         scanf("%f", &presenca);
-    }
-    
-    if(!media){
-        printf("Media não calculada");
-        return 0;
     }
 
     if (media >= 6 && presenca >= 75) {
@@ -105,7 +106,33 @@ int situacao_academica(media) {
 
 int main() {
     
+int opcao_escolhida = menu();
 
-    
+    while(opcao_escolhida != 4){
+        switch(opcao_escolhida){
+            case 1:
+                media = calcular_media();
+
+                opcao_escolhida = menu();
+            break;
+
+            case 2:
+                situacao_academica(media);
+
+                opcao_escolhida = menu();
+            break;
+
+            case 3:
+                calcular_derivada();
+
+                opcao_escolhida = menu();
+            break;
+        }
+    }
+
+    printf("\n==============\n");
+    printf("Sistema encerrado!");
+    printf("\n==============\n");
+
     return 0;
 }
