7f855e18c69cdb6d6e8d63823b09dff2a20c4130
leofigueredo2008-beep <leofigueredo.2008@gmail.com>
2026-04-24 10:26:02 -0300

    Pedindo valores de nota para o usuário

--- src/main.c
+++ src/main.c
@@ -3,6 +3,15 @@
 float media; 
 float nota1, nota2; 
 
+float calcular_media(){
+    printf("Digite a primeira nota: ");
+    scanf("%f", &nota1);
+    printf("Digite a segunda nota: ");
+    scanf("%f", &nota2);
+
+}
+
+
 int main() {
     
 
